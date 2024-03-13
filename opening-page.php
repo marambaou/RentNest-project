39b4252f9662c968475bc922c79ee613816dd861
alphonce maramba <marambaalphonce@gmail.com>
2024-03-13 17:11:26 +0300

    this is the first commit

--- opening-page.php
+++ opening-page.php
@@ -106,6 +106,10 @@
         <a href="house-seeker-log-in.php" id="show">Show All</a>
 
         <hr>
+        <div class="map">
+        <p class="maping">Map navigation</p>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d127641.17044272093!2d36.76499561793435!3d-1.3030359779430214!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f1172d84d49a7%3A0xf7cf0254b297924c!2sNairobi%2C%20Kenya!5e0!3m2!1sen!2sca!4v1710338248195!5m2!1sen!2sca" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
         <div class="about">
             <div class="image">
                 <img src="towfiqu-barbhuiya-joqWSI9u_XM-unsplash.jpg" alt="" class="phot">
